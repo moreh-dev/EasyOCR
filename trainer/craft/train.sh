78a83c20e8c338be47a1c15e4bfb510a67f5fa8b
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-07-19 09:35:58 +0900

    modify the craft train script

--- trainer/craft/train.sh
+++ trainer/craft/train.sh
@@ -1,9 +1,15 @@
 # Create conda environment and install dependencies
 base_env=$(conda info | grep -i 'base environment' | awk -F': ' '{print $2}' | sed 's/ (read only)//' | tr -d ' ')
 env_name=easyocr_detection
-batch_size=$1
-mlflow_uri=$2
+batch_size=110
+mlflow_uri=http://127.0.0.1:5000
 
+while getopts t:m:b:s: flag; do
+    case "${flag}" in
+        b) batch_size=${OPTARG} ;;
+        u) mlflow_uri=${OPTARG} ;;
+    esac
+done
 source ${base_env}/etc/profile.d/conda.sh
 
 if conda env list | grep -q -E "^$env_name\s"; then
@@ -27,15 +33,17 @@ else
     gdown --id 1enVIsgNvBf3YiRsVkxodspOn55PIK-LJ --output "${pretrained_model_file}"
 fi
 
-# Train model
+# Create log_dir to log terminal and peak memory usage
 log_dir="logs_${env_name}"
 mkdir -p $log_dir
 
+# Start mlflow server
 if [[ -x $(command -v mlflow) ]] && [[ -z $(pgrep mlflow) ]]; then
     mlflow server &
     sleep 5
 fi
 
+# Train model
 CUDA_VISIBLE_DEVICES=0 python3 train_mlflow.py --yaml=custom_data_train --batch_size=$batch_size --mlflow_uri=$mlflow_uri >> "${log_dir}/log_terminal.log" 2>&1 &
 # Capture the process ID (PID) of the training script
 pid=$!
